dc6aa1c6366cd107bb87a53a734db3f0859c8113
Jason Miller <mailings-github@millerjason.com>
2026-07-16 23:28:36 -0400

    migrations for neovim 0.12 compatibility

--- lua/plugins/neovimacs.lua
+++ lua/plugins/neovimacs.lua
@@ -4,7 +4,6 @@ return {
     opts = {
       VM_Enabled = vim.g.neovimacs_bindings,
       VM_StartInsert = vim.g.neovimacs_insert,
-      VM_UnixConsoleMetaSendsEsc = false,
       TabIndentStyle = 'none', -- 'emacs', 'never', 'whitespace', 'startofline'
     },
   },
