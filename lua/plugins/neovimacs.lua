bd7c219fef986421b8a9c2e50686bef864c5036a
Jason Miller <mailings-github@millerjason.com>
2026-07-16 23:53:41 -0400

    migrate to lua version of neovimacs

--- lua/plugins/neovimacs.lua
+++ lua/plugins/neovimacs.lua
@@ -1,6 +1,7 @@
 return {
   {
     'millerjason/neovimacs.nvim',
+    branch = 'feature/lua_rewrite',
     opts = {
       VM_Enabled = vim.g.neovimacs_bindings,
       VM_StartInsert = vim.g.neovimacs_insert,
